013c38c3108b548f27939db9a3fc2d0762a14693
Miguel <m.jauregui17@gmail.com>
2016-02-19 16:57:33 -0600

    Checkpoint 32 assignment completed

---
+++ app/controllers/advertisement_controller.rb
@@ -0,0 +1,28 @@
+class AdvertisementController < ApplicationController
+  def index
+    @advertisements = Advertisement.all
+  end
+
+  def show
+    @advertisement = Advertisement.find(params[:id])
+  end
+
+  def new
+    @advertisement = Advertisement.new
+  end
+
+  def create
+    @advertisement = Advertisement.new
+    @advertisement.title = params[:advertisement][:title]
+    @advertisement.copy = params[:advertisement][:copy]
+    @advertisement.price = params[:advertisement][:price]
+
+    if @advertisement.save
+      flash[:notice] = "New ad has been saved"
+      redirect_to @advertisement
+    else
+      flash.now[:alert] = "There was an error saving the ad, please try again"
+      renderm :new
+    end
+  end
+end
