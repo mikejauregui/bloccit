6a537d090542fa07a5695b330a9a746329912492
Miguel <m.jauregui17@gmail.com>
2016-02-26 17:19:10 -0600

    Checkpoint 40 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,11 +34,18 @@ posts = Post.all
   )
 end
 
- user = User.first
- user.update_attributes!(
-   email: 'm.jauregui17@gmail.com',
-   password: 'helloworld'
-   )
+admin = User.create!(
+  name:     'Admin User',
+  email:    'admin@example.com',
+  password: 'helloworld',
+  role:     'admin'
+)
+
+member = User.create!(
+  name:     'Member User',
+  email:    'member@example.com',
+  password: 'helloworld'
+)
 
 puts "Seed finished"
 puts "#{Topic.count} topics created"
