04f41c3d9981c00a71deef078207349c927f2ed8
Miguel <m.jauregui17@gmail.com>
2016-03-01 14:04:12 -0600

    Checkpoint 41 completed

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,11 @@ Rails.application.routes.draw do
     resources :posts, except: [:index]
   end
 
-  resources :users, only: [:new, :create]
+  resources :posts, only: [] do
+     resources :comments, only: [:create, :destroy]
 
+  end
+  resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
 
   get 'about' => 'welcome#about'
