ad8ca4338578ad1d7b8de1b671c5d82cb20df9da
Miguel <m.jauregui17@gmail.com>
2016-04-16 15:41:03 -0500

    Checkpoint 44 assignment completed

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -13,4 +13,14 @@ class FavoriteMailer < ApplicationMailer
 
      mail(to: user.email, bcc: "richard@bloc.io", subject: "New comment on #{post.title}")
    end
+
+   def new_post(post)
+     headers["Message-ID"] = "<posts/#{post.id}@your-app-name.example>"
+     headers["In-Reply-To"] = "<posts/#{post.id}@your-app-name.example>"
+     headers["References"] = "<posts/#{post.id}@your-app-name.example>"
+
+     @post = post
+
+     mail(to: post.user.email, subject: "You're following #{post.title}!")
+   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,8 @@ class Post < ActiveRecord::Base
   has_many :labelings, as: :labelable
   has_many :labels, through: :labelings
   has_many :favorites, dependent: :destroy
+  after_create :create_favorite
+
   default_scope { order('rank DESC') }
 
   validates :title, length: {minimum: 5}, presence: true
@@ -29,5 +31,10 @@ class Post < ActiveRecord::Base
      age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
      new_rank = points + age_in_days
      update_attribute(:rank, new_rank)
+   end
+
+   def create_favorite
+     favorite.create(post: self, user: self.user)
+     FavoriteMailer.new_post(self).deliver_now
    end
 end
