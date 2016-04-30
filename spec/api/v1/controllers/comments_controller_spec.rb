33f2de0083f1a3f74f22122497c88c99c440df02
Miguel <m.jauregui17@gmail.com>
2016-04-30 10:27:38 -0500

    Checkpoint 48 assignment completed

---
+++ spec/api/v1/controllers/comments_controller_spec.rb
@@ -0,0 +1,51 @@
+require 'rails_helper'
+require 'random_data'
+
+ RSpec.describe Api::V1::CommentsController, type: :controller do
+   let(:my_user) { create(:user) }
+   let(:my_topic) { create(:topic) }
+   let(:my_post) { create(:post, topic: my_topic, user: my_user) }
+   let(:my_comment) { Comment.create(body:RandomData.random_paragraph, post: my_post, user: my_user)}
+
+   context "non authenticated users" do
+     it "GET index http status success" do
+       get :index
+       expect(response).to have_http_status(:success)
+     end
+
+     it "GET index http status success" do
+       get :show, id: my_comment.id
+       expect(response).to have_http_status(:success)
+     end
+
+     it "GET show returns a comment" do
+       get :show, id: my_comment.id
+       response_hash = JSON.parse response.body
+       expect(response_hash['id']).to eq my_comment.id
+       expect(response_hash['body']).to eq my_comment.body
+     end
+   end
+
+   context "non authorized users" do
+     before do
+       controller.request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Token.encode_credentials(my_user.auth_token)
+     end
+
+     it "GET index http status success" do
+       get :index
+       expect(response).to have_http_status(:success)
+     end
+
+     it "GET index http status success" do
+       get :show, id: my_comment.id
+       expect(response).to have_http_status(:success)
+     end
+
+     it "GET show returns a comment" do
+       get :show, id: my_comment.id
+       response_hash = JSON.parse response.body
+       expect(response_hash['id']).to eq my_comment.id
+       expect(response_hash['body']).to eq my_comment.body
+     end
+   end
+ end
--- spec/api/v1/controllers/topics_controller_spec.rb
+++ spec/api/v1/controllers/topics_controller_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
  RSpec.describe Api::V1::TopicsController, type: :controller do
    let(:my_user) { create(:user) }
    let(:my_topic) { create(:topic) }
+   let(:my_post) { create(:post, topic: my_topic, user: my_user)}
 
    context "unauthenticated user" do
      it "GET index returns http success" do
@@ -14,13 +15,19 @@ require 'rails_helper'
        get :show, id: my_topic.id
        expect(response).to have_http_status(:success)
      end
+
+     it "GET show returns child posts" do
+       get :show, id: my_topic.id
+       response_hash = JSON.parse response.body
+       expect(response_hash['posts']).to_not be_nil
+     end
    end
 
    context "unauthorized user" do
      before do
        controller.request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Token.encode_credentials(my_user.auth_token)
      end
- 
+
      it "GET index returns http success" do
        get :index
        expect(response).to have_http_status(:success)
@@ -29,6 +36,12 @@ require 'rails_helper'
      it "GET show returns http success" do
        get :show, id: my_topic.id
        expect(response).to have_http_status(:success)
+     end
+
+     it "GET show returns child posts" do
+       get :show, id: my_topic.id
+       response_hash = JSON.parse response.body
+       expect(response_hash['posts']).to_not be_nil
      end
    end
  end
